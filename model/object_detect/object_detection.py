9b9dcdd9d4ff8ca5cd72a90b93ac8afecdc91d76
xueqili02 <xueqili00@gmail.com>
2023-07-04 16:36:18 +0800

    add object detection, own camera

---
+++ model/object_detect/object_detection.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+
+def object_detect():
+    # print(cv2.cuda.getCudaEnabledDeviceCount())
+
+    # Set colors
+    colors = []
+    for i in range(91):
+        random_c = np.random.randint(256, size=3)
+        colors.append((int(random_c[0]), int(random_c[1]), int(random_c[2])))
+
+    # Opencv DNN
+    net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg")
+    # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(size=(320, 320), scale=1 / 255)
+
+    # Load class lists
+    classes = []
+    with open("classes.txt", "r") as file_object:
+        for class_name in file_object.readlines():
+            class_name = class_name.strip()
+            classes.append(class_name)
+
+    # print("Objects list: ")
+    # print(classes)
+
+    # Add the object you want to detect
+    active_objects = ['person', 'cat', 'dog']
+    # print("Active objects: ", active_objects)
+
+    # local camera
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        flag, img_rd = cap.read()
+        ret, frame = flag, img_rd
+
+        # Object Detection
+        (class_ids, scores, bboxes) = model.detect(frame, confThreshold=0.3, nmsThreshold=.4)
+        for class_id, score, bbox in zip(class_ids, scores, bboxes):
+            (x, y, w, h) = bbox
+            class_name = classes[class_id]
+            color = colors[class_id]
+            if class_name in active_objects:
+                face_video = True
+                cv2.putText(img_rd, class_name, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+                cv2.rectangle(img_rd, (x, y), (x + w, y + h), color, 5)
+
+        cv2.imshow("Video", img_rd)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+object_detect()
---
+++ model/object_detect/test.py
@@ -0,0 +1,5 @@
+import cv2
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(cv2.getBuildInformation())
