fb04d003337982d8094bcbc5d3f37a5651fbf4c5
xueqili02 <xueqili00@gmail.com>
2023-07-06 11:46:50 +0800

    split algorithm model and view code in emotion recognition

--- model/emotional_recognition/emo_reco.py
+++ model/emotional_recognition/emo_reco.py
@@ -1,7 +1,7 @@
 import cv2
 import torchvision.transforms as transforms
 import torch
-import EMR
+from model.emotional_recognition.EMR import get_default_device, to_device, MERCnnModel, faceBox, predict_image
 
 
 def emo_reco():
@@ -11,12 +11,12 @@ def emo_reco():
     cam.set(cv2.CAP_PROP_FPS, 30)
 
     # GPU if available, else CPU
-    device = EMR.get_default_device()
+    device = get_default_device()
     print("Selected device:", device)
 
     # Loading pretrained weights
-    w = './model_U.pth'
-    model = EMR.to_device(EMR.MERCnnModel(), device)
+    w = 'model/emotional_recognition/model_U.pth'
+    model = to_device(MERCnnModel(), device)
     if str(device) == 'cpu':
         model.load_state_dict(torch.load(w, map_location=torch.device('cpu')))  # use for cpu
     if str(device) == 'gpu':
@@ -27,7 +27,7 @@ def emo_reco():
     while True:
         _, frame = cam.read()
         if _:
-            bBox = EMR.faceBox(frame)
+            bBox = faceBox(frame)
             if len(bBox) > 0:
                 for box in bBox:
                     x, y, w, h = box
@@ -38,14 +38,16 @@ def emo_reco():
                     except:
                         continue
                     faceExpResizedTensor = transform(faceExpResized)
-                    prediction = EMR.predict_image(faceExpResizedTensor, model, device)
+                    prediction = predict_image(faceExpResizedTensor, model, device)
                     cv2.putText(frame, prediction, (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
-            cv2.imshow('MER', frame)
-        if cv2.waitKey(1) & 0xff == ord('q'):  # to quit the camera press 'q'
-            print('end')
-            break
-    cam.release()
-    cv2.destroyAllWindows()
+
+        yield frame
+        #     cv2.imshow('MER', frame)
+        # if cv2.waitKey(1) & 0xff == ord('q'):  # to quit the camera press 'q'
+        #     print('end')
+        #     break
+    # cam.release()
+    # cv2.destroyAllWindows()
 
 
-emo_reco()
+# emo_reco()
