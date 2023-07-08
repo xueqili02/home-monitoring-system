1e5e57b4be2f63685d7b66e80a16f2fa468dbbd4
xueqili02 <xueqili00@gmail.com>
2023-07-08 12:51:53 +0800

    change url in service app

--- service/urls.py
+++ service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("serviceid/<obj>/<emotion>/<microexpression>/<face>/<caption>/", views.call_service, name="call_service")
+    path("serviceid/<obj>/<emotion>/<microexpression>/<face>/", views.call_service, name="call_service")
 ]
