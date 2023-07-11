f33d8cdedefaf683d56c76159ce01277fcb10d78
xueqili02 <xueqili00@gmail.com>
2023-07-12 00:24:41 +0800

    implement active_objects, record, object_image APIs

--- recognition/urls.py
+++ recognition/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("object_recognition/", views.object_recognition, name="object_recognition"),
-    path("camera/", views.camera, name="camera"),
+    path("camera/<uid>/", views.camera, name="camera"),
     path("range_coordinate/", views.range_coordinate, name="range_coordinate"),
     path("first_image/", views.first_image, name="first_image"),
+    path("active_objects/", views.active_objects, name="active_objects"),
+    path("record/<uid>/", views.record, name="record"),
+    path("object_image/", views.object_image, name="object_image"),
 ]
--- recognition/views.py
+++ recognition/views.py
@@ -1,14 +1,19 @@
 import json
 import cv2
 
+from django.core import serializers
+from urllib.parse import quote_plus
 from django.http import StreamingHttpResponse, HttpResponse
-from model.object_detect.object_detection import object_detection, set_coordinate, get_first_image
+from model.object_detect.object_detection import object_detection, set_coordinate, get_first_image, \
+    set_active_objects, get_record
+from recognition.models import Detection
+from user.models import User
 
 camera_ranges = {}
 
 def object_recognition(request):
     def frame_generator():
-        for frame in object_detection('rtmp://47.92.211.14:1935/live'):
+        for frame, cnt in object_detection('rtmp://47.92.211.14:1935/live'):
             ret, jpeg = cv2.imencode('.jpg', frame)
             frame_data = jpeg.tobytes()
 
@@ -16,13 +21,22 @@ def object_recognition(request):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n\r\n')
     return StreamingHttpResponse(frame_generator(), content_type='multipart/x-mixed-replace; boundary=frame')
 
-def camera(request):
+def camera(request, uid):
     url = request.GET.get("camera_url")
 
     def frame_generator():
-        for frame in object_detection(url):
+        for frame, cnt in object_detection(url):
             ret, jpeg = cv2.imencode('.jpg', frame)
             frame_data = jpeg.tobytes()
+
+            if cnt is not None:
+                user = User.objects.filter(id=uid)
+                if user.exists():
+                    detection_record = get_record()
+                    Detection.objects.create(uid=user[0], time=detection_record.get('time'),
+                                             number=detection_record.get('number'),
+                                             camera_url=detection_record.get('camera_url'),
+                                             path=detection_record.get('path'))
 
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n\r\n')
@@ -48,3 +62,27 @@ def first_image(request):
 
     return HttpResponse(b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n\r\n',
                         content_type='multipart/x-mixed-replace; boundary=frame')
+
+def active_objects(request):
+    body = json.loads(request.body)
+    set_active_objects(body.get('data'))
+    response = {
+        "code": 200,
+        "message": "success",
+        "data": None
+    }
+    return HttpResponse(json.dumps(response))
+
+def record(request, uid):
+    record_queryset = Detection.objects.filter(uid=uid)
+    json_data = serializers.serialize('json', record_queryset)
+    return HttpResponse(json_data, content_type='application/json')
+
+def object_image(request):
+    camera_url = request.GET.get('camera_url')
+    time = request.GET.get('time')
+    path = 'resource/detection_image/' + quote_plus(camera_url) + time + '.jpg'
+    image = cv2.imread(path)
+    ret, jpg = cv2.imencode('.jpg', image)
+    frame_data = jpg.tobytes()
+    return HttpResponse(frame_data, content_type='image/jpeg')
