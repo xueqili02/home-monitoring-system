68f009de70d9aa4a3b2dcb490fcead87bc4ff839
xueqili02 <xueqili00@gmail.com>
2023-07-12 16:20:37 +0800

    modify urls in face app

--- face/urls.py
+++ face/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("upload_image/<uid>/", views.upload_image, name="upload_image"),
+    path("upload_image/uid/<uid>/", views.upload_image, name="upload_image"),
     path("face_login/", views.face_login, name="face_login"),
 ]
