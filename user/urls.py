a40f87bbb218fe10b4a54e136a507fe4811e7570
xueqili02 <xueqili00@gmail.com>
2023-07-13 16:36:09 +0800

    delete logout url

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
 ]
